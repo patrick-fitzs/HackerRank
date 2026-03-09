346014365b5a671540121a9f27b9820da0e9e3fc
Patrick fitzsimons <patrickfitzs17@hotmail.com>
2026-03-09 17:28:40 +0000

    .

--- HackerRank/Bandwidth Allocation.py
+++ HackerRank/Bandwidth Allocation.py
@@ -23,16 +23,19 @@ def findMaximumBandwidths(n, k, m):
     For a given x, the cheapest (minimum-sum) valid array that has a[k]=x
     is a "pyramid" that decreases by 1 per step away from k, but never below 1.
     If that minimum required sum <= m, then x is feasible.
+
+    n: number of endpoints
+    k: highest endpoint index
+    m: total bandwidth
     """
 
     def required_sum(x):
-        left = k - 1        # number of endpoints left of k
-        right = n - k       # number of endpoints right of k
+        # Number of endpoints to the left and right opf l
+        left = k-1 # number of endpoints on the left of k
+        right = n - k # number of endpoints on the right
 
-        # ---- sum on the left side (excluding the center x) ----
         if x > left:
             # We can decrease by 1 all the way to the left edge without hitting 1.
-            # Values: x-1, x-2, ..., x-left
             left_sum = (x - 1 + (x - left)) * left // 2
         else:
             # We hit 1 before the left edge.
@@ -40,7 +43,6 @@ def findMaximumBandwidths(n, k, m):
             left_sum = (x - 1) * x // 2
             left_sum += left - (x - 1)
 
-        # ---- sum on the right side (excluding the center x) ----
         if x > right:
             # Values: x-1, x-2, ..., x-right
             right_sum = (x - 1 + (x - right)) * right // 2
@@ -58,7 +60,7 @@ def findMaximumBandwidths(n, k, m):
         mid = (low + high) // 2
 
         if required_sum(mid) <= m:
-            ans = mid       # mid is feasible, try higher
+            ans = mid  # mid is feasible, try higher
             low = mid + 1
         else:
             high = mid - 1  # mid too big, try lower
@@ -68,5 +70,9 @@ def findMaximumBandwidths(n, k, m):
 
 print(findMaximumBandwidths(6, 2, 11))
 
+
+# k = 2                                     index to have max
+# m = 11                                    total sum
+#   [ _, x=3, _, _, _, _ ]                    n = 6 spaces
 
 #binary search scaling down like a pyramid
